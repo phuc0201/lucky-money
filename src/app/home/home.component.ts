35798a4a26245de92acf82f1ba1040eea3eba3d6
Trịnh Hoàng Phúc <hoangphuc01022002@gmail.com>
2024-01-03 03:13:09 +0700

    update: money

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  isOpen: boolean = true;
+export class HomeComponent implements OnInit {
+  isOpen: boolean = false;
+  imageMoney: string = '';
+  listImage: string[] = ['1', '2', '5', '10'];
+  ngOnInit(): void {
+
+    this.imageMoney = `assets/img/money-${this.listImage[Math.floor(Math.random() * 4)]}.png`;
+  }
 }
